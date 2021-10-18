a8fa316b803ff1f170d71d8e957558edc0abd838
tjtics <tjtics@rit.edu>
2021-10-18 16:26:33 -0400

    Class demos

---
+++ MoreIntroOOP/MoreIntroOOP/Car.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreIntroOOP
+{
+    class Car
+    {
+
+        // variables or data fields must be private at all times
+        private string make;
+        private string model;
+        private string color;
+        private int year;
+
+        // properties
+        public string Make {set; get;}
+        public string Color { 
+            set
+            {
+                if (value != "")
+                    color = value;
+                else
+                    color = "N/A";
+            } 
+            get
+            {
+                return color;
+            }
+        }
+
+        public string Model { set; get; }
+
+        public int Year 
+        { 
+            set 
+            {
+                if (value >= 0)
+                    year = value;
+            }
+            get
+            {
+                return year;
+            }
+        }
+
+        // overloaded constructers
+
+        // called default constructor
+        public Car()
+        {
+            make = "N/A";
+            model = "N/A";
+            color = "N/A";
+            year = 0;
+        }
+        // 5-parameterized CONSTRUCTOR
+        public Car(string _make, string _model, string color, int _year)
+        {
+            make = _make;
+            model = _model;
+            //color = _color;
+            Color = color;
+            year = _year;
+
+        }
+
+        //public void SetColor(string _color)
+        //{
+        //    if (_color != "")
+        //        color = _color;
+        //    else
+        //        color = "N/A";
+        //}
+
+        //public string GetColor()
+        //{
+        //    return color;
+        //}
+
+        public override string ToString()
+        {
+            return $"Make: {Make}\nModel: {Model}\nYear: {Year}\nColor: {Color}";
+        }
+
+        public string GetCarInformation()
+        {
+            return $"Make: {make}\nModel: {model}\nYear: {year}\nColor: {color}";
+        }
+    }
+}
---
+++ MoreIntroOOP/MoreIntroOOP/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace MoreIntroOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] cars = new Car[5];
+            Car tiandre = new Car("Chevrolet", "Avalanche", "Teal", 2006);
+            //car.make = "Chevrolet";
+            //car.model = "Avalanche";
+            //car.year = 2006;
+            //car.color = "Teal";
+            cars[0] = tiandre;
+
+            Car decker = new Car("Toyota", "Prius", "Red", 2010);
+            //decker.make = "Toyota";
+            //decker.model = "Prius";
+            //decker.year = 2010;
+            //decker.color = "Red";
+            cars[1] = decker;
+
+            // directly assign to an element of the array after create a new object.
+            cars[2] = new Car("Subaru", "Outback", "Tungsten", 2019);
+
+            // OIS (Object Initization Syntax)
+            //cars[3] = new Car() { make = "Toyota", model = "Camry", color = "White", year = 2009 };
+
+
+            // Default car
+            Car defaultCar = new Car();
+            defaultCar.Make = "Chevy";
+            defaultCar.Model = "Eqioux";
+            defaultCar.Color = "Red";
+            defaultCar.Year = -300;
+            cars[3] = defaultCar;
+
+            Console.WriteLine($"Make: {defaultCar.Make}\nModel: {defaultCar.Model}\nColor: {defaultCar.Color}\nYear: {defaultCar.Year}");
+
+            //for(int index = 0; index < cars.Length; index++)
+            //{
+            //    Console.WriteLine($"==========Car #{index +1}==========\n{cars[index].GetCarInformation()}");
+            //}
+
+            foreach (Car car in cars)
+            {
+                if (car != null)
+                    Console.WriteLine($"{car}\n");
+                
+            }
+        }
+
+        // overloaded methods
+        public void Display()
+        {
+
+        }
+
+        public void Display(string message)
+        {
+
+        }
+    }
+}
